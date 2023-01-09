66a438a3a26e9aa5d29c019c5a5749702b1fa8f4
Nilam <Nilam.Nanaware47@gmail.com>
2023-01-09 19:40:05 +0530

    PSET-3.py commited

---
+++ strings-PSETS.mv/PSET-3/PSET-3.py
@@ -0,0 +1,4 @@
+# Write a `python script` that captures USN of a student from std. input and converts all lowercase letters to uppercase and prints the same.
+
+usn_name= input("Enter the USN of a student \n")
+print("Ouput after converting lowercase letters to uppercase: \n", usn_name.upper())
