fd83cfae5deba7890adb8d36d7843f1a6faec7f2
Nilam <Nilam.Nanaware47@gmail.com>
2023-01-09 19:56:13 +0530

    PSET-4.py commited

---
+++ strings-PSETS.mv/PSET-4/PSET-4.py
@@ -0,0 +1,10 @@
+#In the string hakuna matata print the substring tata using slicing. Try with both positive and negative indices
+
+#Taking hakuna matata as a input as a string.
+
+str_name = "hakuna matata"
+
+#printing subtring  tata using slicing (Positive indices)
+
+print("printing subtring  tata using slicing (Positive indices)\n",str_name[9:])
+print("printing subtring  tata using slicing (negative indices)\n",str_name[-4:])
