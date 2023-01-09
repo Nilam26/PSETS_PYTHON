5c20035455f6a0797c2819171aef53c566fa1cf4
Nilam <Nilam.Nanaware47@gmail.com>
2023-01-09 20:09:53 +0530

    PSET-6.md and PSET-6.py files comitted

---
+++ strings-PSETS.mv/PSET-6/PSET-6.py
@@ -0,0 +1,6 @@
+#Write a python script that reads the first and last name of a person and returns the full name.
+
+first_name = input("Enter the first name \n")
+last_name = input("Enter the first name \n")
+
+print("full name of a person is :"," ", first_name,last_name)
